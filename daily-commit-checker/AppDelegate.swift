9f58284828a5eff37cac5e390afd347dc757d176
larryjung <corbswjd@gmail.com>
2018-10-21 21:45:35 +0900

    first commit

---
+++ daily-commit-checker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  daily-commit-checker
+//
+//  Created by chaegyun jung on 2018. 10. 21..
+//  Copyright © 2018년 chaegyun jung. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ daily-commit-checker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  daily-commit-checker
+//
+//  Created by chaegyun jung on 2018. 10. 21..
+//  Copyright © 2018년 chaegyun jung. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
